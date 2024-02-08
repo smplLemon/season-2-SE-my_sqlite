8c3ac0d3b6424b56d096a1b60fe89b891204c54a
Shoxruhxon <ismoilovshoxruh120@gmail.com>
2024-02-08 23:24:02 +0500

    my_sqlite

--- my_sqlite_cli.rb
+++ my_sqlite_cli.rb
@@ -1,56 +1,70 @@
-require 'csv'
 require './my_sqlite_request.rb'
+require 'csv'
 
-def parse_command_sort(command)
-  tokens = command.split
-  db_sqlite = MySqliteRequest.new
-  save_title = ""
+def func_for_sorting(command)
+    db_sqlite = MySqliteRequest.new
+    string_value = command.include?("VALUES") ? command.split('VALUES')[1].strip : nil
+    command = command.split(" ")
+    @save_title = ""
 
-  tokens.each_with_index do |token, index|
-    case token
-    when "SELECT"
-      analysis_save = tokens[index + 1].include?(",") ? tokens[index + 1].split(",") : tokens[index + 1]
-      db_sqlite = db_sqlite.select(analysis_save)
-    when "FROM"
-      db_sqlite = db_sqlite.from(tokens[index + 1])
-    when "WHERE"
-      where_clause = command.split("WHERE").last.split("=").map(&:strip)
-      db_sqlite = db_sqlite.where(where_clause[0], where_clause[1].gsub(/^'|'$/, ''))
-    when "INSERT"
-      if tokens[index + 1] == "INTO"
-        save_title = tokens[index + 2]
-        db_sqlite = db_sqlite.insert(save_title)
-      end
-    when "UPDATE"
-      db_sqlite = db_sqlite.update(tokens[index + 1])
-      puts tokens[index + 1]
-    when "DELETE"
-      db_sqlite = db_sqlite.delete
-    when "VALUES"
-      save_title = tokens[index - 1] if tokens[index - 1] == "INTO"
-      values = File.read(save_title).split("\n")[0] << "\n" << command.split("VALUES")[1].gsub(/[()]/, '')
-      arr_data = CSV.parse(values, headers: true).map(&:to_h)
-      db_sqlite = db_sqlite.values(arr_data[0])
-    when "SET"
-      my_sstr = ""
-      (index + 1...tokens.length).each do |i|
-        break if tokens[i] == "WHERE"
-        my_sstr << tokens[i] << " "
-      end
-      for_hashing = Hash[my_sstr.scan(/(\w+)\s*=\s*'([^']+)'/)]
-      db_sqlite = db_sqlite.values(for_hashing)
+    for i in 0...command.length
+        case command[i]
+        when "SELECT"
+            analysis_save = command[i + 1].include?(",") ? command[i + 1].split(",") : command[i + 1]
+            db_sqlite = db_sqlite.select(analysis_save)
+        when "FROM"
+            db_sqlite = db_sqlite.from(command[i + 1])
+        when "WHERE"
+            analysis_save = command.join(" ").split("WHERE").last.split("=").map(&:strip)
+            db_sqlite = db_sqlite.where(analysis_save[0], analysis_save[1].gsub(/^'|'$/, ''))
+        when "INSERT"
+            if command[i + 1] == "INTO"
+                i += 2
+                @save_title = command[i]
+                db_sqlite = db_sqlite.insert(command[i])
+            end
+        when "UPDATE"
+            db_sqlite = db_sqlite.update(command[i + 1])
+            puts command[i + 1]
+        when "DELETE"
+            db_sqlite = db_sqlite.delete()
+        when "VALUES"
+            i += 1
+            in_val = File.read(@save_title).split("\n")[0]
+            in_val << "\n"
+            in_val << string_value.gsub(/[()]/, '')
+            arr_data = CSV.parse(in_val, headers: true).map(&:to_h)
+            db_sqlite = db_sqlite.values(arr_data[0])
+        when "SET"
+            i += 1
+            my_sstr = ""
+            x = i
+            for x in 0..(command.length - 1)
+                break if command[x] == "WHERE"
+                my_sstr << command[x]
+                my_sstr << " "
+              end
+            for_hashing = {}
+            my_sstr.scan(/(\w+)\s*=\s*'([^']+)'/) do |name_col, val|
+                for_hashing[name_col] = val
+            end
+            db_sqlite = db_sqlite.values(for_hashing)
+        end
     end
-  end
-  db_sqlite.run
+    db_sqlite.run
 end
+
 
 def ruby_main
-  while get_readed = $stdin.gets
-    break if get_readed.include?("quit")
-    parse_command_sort(get_readed)
-    print "my_sqlite_cli>"
-  end
+    while get_readed = $stdin.gets
+        if get_readed.include?("quit")
+            return
+        end
+        func_for_sorting(get_readed)
+        print "my_sqlite_cli>"
+    end
 end
+
 
 print "my_sqlite_cli>"
 ruby_main
